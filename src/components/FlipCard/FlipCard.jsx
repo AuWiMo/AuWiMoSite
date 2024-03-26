f04d5027d488bde0926903dc6cd5bcc5be24cff0
AuWiMo <yodacanfly@gmail.com>
2024-03-26 11:16:50 -0400

    mild code formatting

--- src/components/FlipCard/FlipCard.jsx
+++ src/components/FlipCard/FlipCard.jsx
@@ -8,15 +8,15 @@ const FlipCard = ({ name, role, dates, description, short_desc, image }) => {
     <div className='card_margins'>
       <div className="flip-container" onTouchStart={() => {this.classList.toggle('hover');}}>
       <div className="flipper">
+
         <div className="front matrix_font">
           <div className="project_name">{name}</div>
           <div>{short_desc}</div>
           <img src={image} alt='logo for this business' className='project_image' draggable='false'/>
           <div >{role}</div>
           <div className='info_icon matrix_font'>ⓘ</div>
-
-          
         </div>
+
         <div className="back matrix_font">
           <div>
             <div className="project_name">{name}</div>
@@ -25,6 +25,7 @@ const FlipCard = ({ name, role, dates, description, short_desc, image }) => {
           </div>
           <div className='project_description'>{description}</div>
         </div>
+        
       </div>
       
     </div>
